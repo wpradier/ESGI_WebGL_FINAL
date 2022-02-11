6a8934b013aee46f8f16b3c51092265bdb9345fd
marina robert <marina.robert1508@gmail.com>
2022-02-11 16:38:05 +0100

    add comments

--- public/javascripts/pointLight.js
+++ public/javascripts/pointLight.js
@@ -3,8 +3,10 @@ import * as THREE from "/modules/three.js-master/build/three.module.js"
 async function createPointLight(statueObj, scene) {
     let sphere, pointLightRight, pointLightLeft;
 
-    sphere = new THREE.SphereGeometry( 2, 16, 8 ); // radius (par défaut 1), nb de segments horizontaux, nb de segments verticaux
+    // radius (default 1), nb of horizontal segments, nb of vertical segments
+    sphere = new THREE.SphereGeometry( 2, 16, 8 );
 
+    // change color, intensity, distance
     pointLightRight = new THREE.PointLight( 0xff0040, 2, 50 );
     pointLightRight.position.set(statueObj.obj.position.x - 220, statueObj.obj.position.y + 338, statueObj.obj.position.z - 475); // position (x, z, y)
     pointLightRight.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
